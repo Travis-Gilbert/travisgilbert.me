5e5ba87b62cb38ac661d3aefa28367afb4411801
Travis Gilbert <82787025+Travis-Gilbert@users.noreply.github.com>
2026-03-15 03:18:19 -0400

    Delete research_api directory

--- research_api/apps/paper_trail/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaperTrailConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.paper_trail'
-    verbose_name = 'Paper Trail'
